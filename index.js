322aa1f3e6589fc2bd46e74fb92dc49cc471ee8d
c3dr0x <cedric.bouletkessler@gmail.com>
2018-06-30 19:36:00 +0200

    style: Add prettier configuration

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
 module.exports = {
-    'parser': 'babel-eslint',
-    'parserOptions': {
-        'ecmaVersion': 6,
-        'sourceType': 'module',
-        'ecmaFeatures': {
-            'experimentalObjectRestSpread': true,
-            'jsx': true
-        },
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true
+        }
     },
     settings: {
-        'import/extensions': ['.js', '.jsx'],
-        'import/resolver': {
+        "import/extensions": [".js", ".jsx"],
+        "import/resolver": {
             node: {
                 // Note: will not complain if only _one_ of these files exists.
-                extensions: ['.js', '.jsx'],
-                "webpack": "webpack.config.js"
+                extensions: [".js", ".jsx"],
+                webpack: "webpack.config.js"
             }
         }
     },
-    'extends': ['eslint:recommended'],
-    'plugins': [
-        'eslint-plugin-react',
-        'eslint-plugin-filenames',
-        'eslint-plugin-import'
-    ],
-    'globals': {
-        'Promise': true,
+    extends: ["eslint:recommended"],
+    plugins: ["eslint-plugin-react", "eslint-plugin-filenames", "eslint-plugin-import"],
+    globals: {
+        Promise: true
     },
-    'env': {
-        'browser': true,
-        'node': true
+    env: {
+        browser: true,
+        node: true
     },
-    'rules': {
+    rules: {
         /* Import Plugin (recommended conf)*/
         // analysis/correctness
         // Check if import can be resolved on file system
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
-        'import/no-unresolved': 'warning', // In case of alias, this should not be an error
+        "import/no-unresolved": "warning", // In case of alias, this should not be an error
         // Check if the import is correctly done, according to the named export, or the default one
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/named.md
-        'import/named': 'error',
+        "import/named": "error",
         // Check if the import * is done correctly, ie that the named import used after do exists
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/namespace.md
-        'import/namespace': 'error',
+        "import/namespace": "error",
         // Check if an import using default as been exported the same way
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/default.md
-        'import/default': 'error',
+        "import/default": "error",
         // Check if the export are done properly (no multiple default, or named one)
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/export.md
-        'import/export': 'error',
+        "import/export": "error",
         // red flags (thus, warnings)
         // Warning if the default export is imported the name of a named export
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
-        'import/no-named-as-default': 'warn',
+        "import/no-named-as-default": "warn",
         // Warning if the import is made on default, then used a member instead of named import
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default-member.md
-        'import/no-named-as-default-member': 'warn',
+        "import/no-named-as-default-member": "warn",
         // Warning if duplicates imports are made on the same module
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-        'import/no-duplicates': 'warn',
+        "import/no-duplicates": "warn",
         // Warning if file has no import or export. See rule for Detail
         // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/unambiguous.md
-        'import/unambiguous': 'warn',
+        "import/unambiguous": "warn",
         /* Filename Plugin */
         // Warning if the filename does not match the kebab case (a-z\-)
-        'filenames/match-regex': ['warn', '^[a-z\-]+$'],
+        "filenames/match-regex": ["warn", "^[a-z-]+$"],
         //'filenames/match-exported': ['error', 'kebab'],
         //'filenames/no-index': 'error', // Could be a good idea
 
@@ -81,9 +77,9 @@ module.exports = {
         "react/no-access-state-in-setstate": "warn",
         // Warn if an element uses an Array index in its key.
         "react/no-array-index-key": "warn",
-        // This rule helps prevent problems caused by using children and the dangerouslySetInnerHTML prop at the same time. 
+        // This rule helps prevent problems caused by using children and the dangerouslySetInnerHTML prop at the same time.
         "react/no-danger-with-children": "error",
-        // Several methods are deprecated between React versions. This rule will warn you if you try to use a deprecated method. 
+        // Several methods are deprecated between React versions. This rule will warn you if you try to use a deprecated method.
         "react/no-deprecated": "warn",
         // NEVER mutate this.state directly, as calling setState() afterwards may replace the mutation you made. Treat this.state as if it were immutable.
         "react/no-direct-mutation-state": "error",
@@ -127,172 +123,175 @@ module.exports = {
         /* Comments */
         // Warning if no comments
         // http://eslint.org/docs/rules/require-jsdoc
-        'require-jsdoc': ['warn', {
-            'require': {
-                'FunctionDeclaration': true,
-                'MethodDefinition': true,
-                'ClassDeclaration': false, // Disabling because of decorators
-                'ArrowFunctionExpression': false // Documenting lambda is too much
+        "require-jsdoc": [
+            "warn",
+            {
+                require: {
+                    FunctionDeclaration: true,
+                    MethodDefinition: true,
+                    ClassDeclaration: false, // Disabling because of decorators
+                    ArrowFunctionExpression: false // Documenting lambda is too much
+                }
             }
-        }],
+        ],
         // Warning uncomplete comments
         // http://eslint.org/docs/rules/require-jsdoc
-        'valid-jsdoc': [
-            'warn',
+        "valid-jsdoc": [
+            "warn",
             {
-                'requireParamDescription': true,
-                'requireReturn': false
+                requireParamDescription: true,
+                requireReturn: false
             }
         ],
         /* General */
         // Error if not using simple quotes
         // http://eslint.org/docs/rules/quotes
-        'quotes': ['error', 'single', 'avoid-escape'],
+        quotes: ["error", "single", "avoid-escape"],
         // Error if not using ===
         // http://eslint.org/docs/rules/eqeqeq
-        eqeqeq: ['error', 'always', { null: 'ignore' }],
-        'strict': 'error',
+        eqeqeq: ["error", "always", { null: "ignore" }],
+        strict: "error",
         // Error if using not camelcase variables
         // http://eslint.org/docs/rules/camelcase
-        'camelcase': 'error',
+        camelcase: "error",
         // Allowing _ in identifier
         // http://eslint.org/docs/rules/no-underscore-dangle
-        'no-underscore-dangle': 'off',
+        "no-underscore-dangle": "off",
         // Constructor should begin with capital letter
         // http://eslint.org/docs/rules/new-cap
-        'new-cap': 'warn',
+        "new-cap": "warn",
         // Error if not using single quotes in JSX
         // http://eslint.org/docs/rules/jsx-quotes
-        'jsx-quotes': ['error', 'prefer-single'],
+        "jsx-quotes": ["error", "prefer-single"],
         // Error if trailing comma
         // http://eslint.org/docs/rules/comma-dangle
-        'comma-dangle': 'error',
+        "comma-dangle": "error",
         // Error if using var (prefer let or cons)
         // http://eslint.org/docs/rules/no-var
-        'no-var': 'error',
+        "no-var": "error",
         // Indentation (4 spaces)
         // http://eslint.org/docs/rules/indent
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        indent: ["error", 4, { SwitchCase: 1 }],
         // Brace style (same line)
         // http://eslint.org/docs/rules/brace-style
-        'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+        "brace-style": ["error", "1tbs", { allowSingleLine: true }],
         // Comma after element in array, etc
         // http://eslint.org/docs/rules/comma-style
-        'comma-style': ['error', 'last'],
+        "comma-style": ["error", "last"],
         // Function should always be named
         // http://eslint.org/docs/rules/func-names
-        'func-names': 'error',
+        "func-names": "error",
         // Spacing around colon
         // http://eslint.org/docs/rules/key-spacing
-        'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+        "key-spacing": ["error", { beforeColon: false, afterColon: true }],
         // Maximum nested callbacks
         // http://eslint.org/docs/rules/max-nested-callbacks
-        'max-nested-callbacks': ['warn', 3],
+        "max-nested-callbacks": ["warn", 3],
         // Avoid multiple empty lines (max 2)
         // http://eslint.org/docs/rules/no-multiple-empty-lines
-        'no-multiple-empty-lines': 'warn',
+        "no-multiple-empty-lines": "warn",
         // Quote props only if needed
         // http://eslint.org/docs/rules/quote-props
-        'quote-props': ['warn', 'as-needed'],
+        "quote-props": ["warn", "as-needed"],
         // Do not use variable before they are defined
         // http://eslint.org/docs/rules/no-use-before-define
-        'no-use-before-define': 'error',
+        "no-use-before-define": "error",
         // Switch must have default case, even if empty
         // http://eslint.org/docs/rules/default-case
-        'default-case': 'error',
+        "default-case": "error",
         // Prefer foo.bar instead of foo["bar"]
         // http://eslint.org/docs/rules/dot-notation
-        'dot-notation': 'warn',
+        "dot-notation": "warn",
         // Alert should not be used
         // http://eslint.org/docs/rules/no-alert
-        'no-alert': 'warn',
+        "no-alert": "warn",
         // Allowing console logging
         // http://eslint.org/docs/rules/no-console
-        'no-console': 'warn',
+        "no-console": "warn",
         // Eval should not be used in most case
         // http://eslint.org/docs/rules/no-eval
-        'no-eval': 'error',
+        "no-eval": "error",
         // No useless block
         // http://eslint.org/docs/rules/no-lone-blocks
-        'no-lone-blocks': 'error',
+        "no-lone-blocks": "error",
         // No multi spaces
         // http://eslint.org/docs/rules/no-multi-spaces
-        'no-multi-spaces': 'error',
+        "no-multi-spaces": "error",
         // No reassign to window or process
         // http://eslint.org/docs/rules/no-global-assign
-        'no-global-assign': 'error',
+        "no-global-assign": "error",
         // No multi line string (use backquote instead of \ in end of line)
         // http://eslint.org/docs/rules/no-multi-str
-        'no-multi-str': 'error',
+        "no-multi-str": "error",
         // No primitive wrapper
         // http://eslint.org/docs/rules/no-new-wrappers
-        'no-new-wrappers': 'error',
+        "no-new-wrappers": "error",
         // No variable redeclare
         // http://eslint.org/docs/rules/no-redeclare
-        'no-redeclare': 'error',
+        "no-redeclare": "error",
         // No return with an assign
         // http://eslint.org/docs/rules/no-return-assign
-        'no-return-assign': 'error',
+        "no-return-assign": "error",
         // No comparing a variable with itself
         // http://eslint.org/docs/rules/no-self-compare
-        'no-self-compare': 'error',
+        "no-self-compare": "error",
         // Throw error or exception, not litterals
         // http://eslint.org/docs/rules/no-throw-literal
-        'no-throw-literal': 'error',
+        "no-throw-literal": "error",
         // No unused expressions
         // http://eslint.org/docs/rules/no-unused-expressions
-        'no-unused-expressions': 'error',
+        "no-unused-expressions": "error",
         // No TODO or fixme
         // http://eslint.org/docs/rules/no-warning-commentss
-        'no-warning-comments': ['warn', { 'terms': ['todo', 'fixme'], 'location': 'anywhere' }],
+        "no-warning-comments": ["warn", { terms: ["todo", "fixme"], location: "anywhere" }],
         // Use radix in parseInt
         // http://eslint.org/docs/rules/radix
-        'radix': 'error',
+        radix: "error",
         // Wrap immediate function call in parenthesis
         // http://eslint.org/docs/rules/wrap-iife
-        'wrap-iife': 'error',
+        "wrap-iife": "error",
         // No constant condition
         // http://eslint.org/docs/rules/no-constant-condition
-        'no-constant-condition': 'error',
+        "no-constant-condition": "error",
         // No duplicate arguments
         // http://eslint.org/docs/rules/no-dupe-args
-        'no-dupe-args': 'error',
+        "no-dupe-args": "error",
         // No duplicate keys in object
         // http://eslint.org/docs/rules/no-dupe-key
-        'no-dupe-keys': 'error',
+        "no-dupe-keys": "error",
         // No duplicate case in switch
         // http://eslint.org/docs/rules/no-duplicate-case
-        'no-duplicate-case': 'error',
+        "no-duplicate-case": "error",
         // No empty statement
         // http://eslint.org/docs/rules/no-empty
-        'no-empty': 'error',
+        "no-empty": "error",
         // Error on extra semi colon
         // http://eslint.org/docs/rules/no-extra-semi
-        'no-extra-semi': 'error',
+        "no-extra-semi": "error",
         // No reassign on function
         // http://eslint.org/docs/rules/no-func-assign
-        'no-func-assign': 'error',
+        "no-func-assign": "error",
         // No sparse arrays
         // http://eslint.org/docs/rules/no-sparse-arrays
-        'no-sparse-arrays': 'error',
+        "no-sparse-arrays": "error",
         // No unreachable code
         // http://eslint.org/docs/rules/no-unreachable
-        'no-unreachable': 'error',
+        "no-unreachable": "error",
         // Use isNan, do not compare NaN
         // http://eslint.org/docs/rules/use-isnan
-        'use-isnan': 'error',
+        "use-isnan": "error",
         // Ensure typeof literal is correct
         // http://eslint.org/docs/rules/valid-typeof
-        'valid-typeof': 'error',
+        "valid-typeof": "error",
         // Ensure spacing around block
         // http://eslint.org/docs/rules/space-before-blocks
-        'space-before-blocks': 'error',
+        "space-before-blocks": "error",
         // Ensure spacing around fat arrow
         // http://eslint.org/docs/rules/arrow-spacing
-        'arrow-spacing': 'error',
+        "arrow-spacing": "error",
         // Ensure spacing around keywords
-        'keyword-spacing': 'error',
+        "keyword-spacing": "error",
         // Ensure that function parameter are not reassigned
-        'no-param-reassign': ["error", { "props": true }]
+        "no-param-reassign": ["error", { props: true }]
     }
 };
